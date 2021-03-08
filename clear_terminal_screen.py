7b5ee891fbdf8740cd25f6c370d11f5de431dbb1
wozza0212 <71821573+wozza0212@users.noreply.github.com>
2021-03-08 22:38:48 +0000

    Add files via upload

---
+++ clear_terminal_screen.py
@@ -0,0 +1,12 @@
+import os
+
+# Only works when running through the terminal!
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+print('jcbnkjbncvksnv')
+print('hjcbnksdjcn')
+cls()
+print('did it work, i cant look')
